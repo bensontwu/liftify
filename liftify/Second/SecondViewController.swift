3c44f949da4357cdc106e74d88dba24ded802898
benson <bensontwu@benson-mbp.local>
2019-07-14 14:36:35 -0700

    Submit button appearance logic finished

--- liftify/Second/SecondViewController.swift
+++ liftify/Second/SecondViewController.swift
@@ -8,13 +8,61 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITextFieldDelegate {
 
+    // MARK: - Properties
+    
+    @IBOutlet var liftButtons: [UIButton]!
+    @IBOutlet var selectedLiftLabel: UILabel!
+    @IBOutlet var weightTextField: UITextField!
+    @IBOutlet var repsTextField: UITextField!
+    @IBOutlet var submitSetButton: UIButton!
+    
+    // MARK: - View Setup
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        weightTextField.delegate = self
+        repsTextField.delegate = self
+        
     }
-
-
+    
+    
+    // MARK: - Button Actions
+    
+    @IBAction func selectALiftTapped(_ sender: Any) {
+        for button in liftButtons {
+            UIView.animate(withDuration: 0.3) {
+                button.isHidden = !button.isHidden
+                self.view.layoutIfNeeded() // tidy up animation
+            }
+        }
+    }
+    
+    @IBAction func liftTapped(_ sender: UIButton) {
+        repsTextField.isHidden = false
+        weightTextField.isHidden = false
+        selectedLiftLabel.isHidden = false
+        if let selectedLift = sender.titleLabel?.text {
+            selectedLiftLabel.text = "Best \(selectedLift) Set:"
+        }
+    }
+    
+    @IBAction func submitSetButtonTapped(_ sender: Any) {
+        print("YOUR SET")
+        print("weight: \(weightTextField.text)")
+        print("reps: \(repsTextField.text)")
+    }
+    
+    
+    // MARK: - TextFieldDelegate Methods
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if weightTextField.text != nil && repsTextField.text != nil {
+            submitSetButton.isHidden = false
+        }
+        return true
+    }
+    
 }
-
---
+++ liftify/Set.swift
@@ -0,0 +1,19 @@
+//
+//  Set.swift
+//  Liftify
+//
+//  Created by benson on 7/9/19.
+//  Copyright © 2019 wu. All rights reserved.
+//
+
+import Foundation
+
+class Set {
+    var weight: Int
+    var reps: Int
+    
+    init(weight: Int, reps: Int) {
+        self.weight = weight
+        self.reps = reps
+    }
+}
